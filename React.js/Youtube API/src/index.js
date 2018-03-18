eb35add17612320b2e194dad73843e3504beaf15
Aditya Verma <adi004.zz@gmail.com>
2018-03-18 23:50:06 +0530

    Added YT search lib, and a working example

--- React.js/Youtube API/src/index.js
+++ React.js/Youtube API/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyAMvVt5tcoszNHkADUBbihjAAh0417dUpc";
 
+YTSearch({key: API_KEY, term: 'cool'}, function(data){
+    console.log(data);
+});
 const APP = () => {
     return (
         <div>
