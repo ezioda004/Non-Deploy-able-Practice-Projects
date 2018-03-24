fb7c7b49d6d0442e57af19433871afe0c96b69ce
Aditya Verma <adi004.zz@gmail.com>
2018-03-24 22:54:40 +0530

    Fully working app

--- React.js/Youtube API/src/index.js
+++ React.js/Youtube API/src/index.js
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyAMvVt5tcoszNHkADUBbihjAAh0417dUpc";
 
 
-class App extends React.Component  {
+class App extends Component  {
     constructor(props){
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: 'cool'}, (videos) =>{
-            this.setState({videos: videos});
-            console.log(videos);
+        this.videoSearch('cool');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) =>{
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
+
     render(){
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
+                <VideoDetail video = {this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                    videos = {this.state.videos} />
             </div>
         );
     }
     
 }
 
-ReactDOM.render(<SearchBar/>, document.querySelector(".container"));
+ReactDOM.render(<App/>, document.querySelector(".container"));
 
